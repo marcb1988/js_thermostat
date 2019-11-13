1bab624eb60e204728bca33fa6300fffb31dfd61
Marc Blenkinsop <marcblenky@hotmail.com>
2019-11-13 16:24:20 +0000

    energy usage display tests added failing

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -70,4 +70,30 @@ describe('thermostat', function () {
             expect(thermostat.getCurrentTemperature()).toEqual(32);
         });
     });
+    describe('displaying usage levels', function () {
+        describe('when the temperature is below 18 degrees', function () {
+            it('it is considered low-usage', function () {
+                for (var i = 0; i < 3; i++) {
+                    thermostat.down();
+                }
+                expect(thermostat.energyUsage()).toEqual('low-usage');
+            });
+        });
+
+        describe('when the temperature is between 18 and 25 degrees', function () {
+            it('it is considered medium-usage', function () {
+                expect(thermostat.energyUsage()).toEqual('medium-usage');
+            });
+        });
+
+        describe('when the temperature is anything else', function () {
+            it('it is considered high-usage', function () {
+                thermostat.switchPowerSavingModeOff();
+                for (var i = 0; i < 6; i++) {
+                    thermostat.up();
+                }
+                expect(thermostat.energyUsage()).toEqual('high-usage');
+            });
+        });
+    });
 });
