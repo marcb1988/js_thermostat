2d23e5a6076e742e2eda33bf17667bce85cc94b0
Marc Blenkinsop <marcblenky@hotmail.com>
2019-11-13 16:32:26 +0000

    energy usage display tests added passing

--- src/Thermostat.js
+++ src/Thermostat.js
@@ -4,6 +4,7 @@ function Thermostat() {
     this.MAX_LIMIT_PSM_ON = 25;
     this.MAX_LIMIT_PSM_OFF = 32;
     this.MINIMUM_TEMPERATURE = 10;
+    this.MEDIUM_ENERGY_USAGE_LIMIT = 18;
     this.DEFAULT_TEMPERATURE = 20;
     this.temperature = this.DEFAULT_TEMPERATURE;
     this.powerSavingMode = true;
@@ -51,4 +52,14 @@ Thermostat.prototype.switchPowerSavingModeOn = function () {
 
 Thermostat.prototype.resetTemperature = function () {
     this.temperature = this.DEFAULT_TEMPERATURE;
-}
+};
+
+Thermostat.prototype.energyUsage = function () {
+    if (this.temperature < this.MEDIUM_ENERGY_USAGE_LIMIT) {
+        return 'low-usage';
+    }
+    if (this.temperature >= this.MEDIUM_ENERGY_USAGE_LIMIT && this.temperature <= this.MAX_LIMIT_PSM_ON) {
+        return 'medium-usage';
+    }
+    return 'high-usage';
+};
